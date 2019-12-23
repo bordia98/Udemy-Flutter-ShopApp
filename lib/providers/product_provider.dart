342bb35a23bba3306a2c5349e0085b5686a496f6
bordia98 <bhavyabordia@gmail.com>
2019-12-23 14:03:19 +0530

    adding product to firebase added

--- lib/providers/product_provider.dart
+++ lib/providers/product_provider.dart
@@ -1,6 +1,10 @@
+import 'dart:convert';
+import 'dart:core';                                                  
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import '../dummy_data.dart';
 import './product.dart';
+
 
 class ProductProvider with ChangeNotifier{
   List<Product> _products = dummyProduct;
@@ -23,21 +27,60 @@ class ProductProvider with ChangeNotifier{
     );
   }
 
-  void addProduct(Product pro){
+  Future<void> addProduct(Product pro) async{
+    
+    final url = 'https://flutter-shop-app-725c6.firebaseio.com/products.json';
+    
     if(pro.id == null){
-      final toadd = Product(
-        id: DateTime.now().toString(),
-        title: pro.title,
-        description: pro.description,
-        imageUrl: pro.imageUrl,
-        price: pro.price
-      );
-      _products.add(toadd);
+      try{
+        final response = await http.post(url,
+          body: jsonEncode({
+            "title": pro.title,
+            "description":pro.description,
+            "imageUrl": pro.imageUrl,
+            "price": pro.price,
+            "isFavorite": pro.isFavorite
+          })
+        );
+        print("*********************");
+        print(json.decode(response.body));
+        final toadd = Product(
+          id: json.decode(response.body)['name'],
+          title: pro.title,
+          description: pro.description,
+          imageUrl: pro.imageUrl,
+          price: pro.price
+        );
+        _products.add(toadd);
+        notifyListeners();
+      }catch(error){
+        print("yaha aaya hai error");
+        print(error.toString());
+        throw error;
+      }
     }else{
-      var index =  _products.indexOf(_products.firstWhere((val)=> val.id == pro.id));
-      _products[index] = pro; 
+      final updateurl = "https://flutter-shop-app-725c6.firebaseio.com/products/${pro.id}.json";
+      try{
+        final res = await http.patch(updateurl,
+          body: jsonEncode({
+              "title":pro.title,
+              "description": pro.description,
+              "price":pro.price,
+              "imageUrl":pro.imageUrl
+            })
+          );
+
+        if(res.statusCode >=400){
+          throw Exception("Error encountered while updating");
+        }
+        var index =  _products.indexOf(_products.firstWhere((val)=> val.id == pro.id));
+        _products[index] = pro; 
+        notifyListeners();
+      }catch(error){
+        print(error.toString());
+        throw error;
+      }
     }
-    notifyListeners();
   }
 
   void deleteProduct(String id){
--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -18,6 +18,7 @@ class _EditProductScreenState extends State<EditProductScreen> {
   final _urlfocus = FocusNode();
   final _imagecontroller = TextEditingController();
   final _formkey = GlobalKey<FormState>();
+  bool _isloading = false;
   var _tempproduct = Product(
     id: null,
     description: "",
@@ -51,15 +52,26 @@ class _EditProductScreenState extends State<EditProductScreen> {
     super.dispose();
   }
 
-  void _saveproduct(){
+  Future<void> _saveproduct() async{
     var valid = _formkey.currentState.validate();
     if(!valid){
       return;
     }
+    setState(() {
+      _isloading = true;
+    });
     _formkey.currentState.save();
     final productData = Provider.of<ProductProvider>(context,listen: false);
-    productData.addProduct(_tempproduct);
-    Navigator.of(context).pushNamedAndRemoveUntil("/userproduct", ModalRoute.withName("/"));
+    try{
+      await productData.addProduct(_tempproduct);
+    }catch(error){
+      print(error);
+    }finally{
+      setState(() {
+        _isloading = false;
+      });
+      Navigator.of(context).pushNamedAndRemoveUntil("/userproduct", ModalRoute.withName("/"));
+    }
   }
 
   @override
@@ -89,193 +101,205 @@ class _EditProductScreenState extends State<EditProductScreen> {
       appBar: AppBar(
         title: Text("Edit Product"),
       ),
-      body: Form(
-        key: _formkey,
-        child: SingleChildScrollView(
-          padding: EdgeInsets.all(10),
-          child: Column(
-            children: <Widget>[
-              Container(
-                child: Card(
+      body: _isloading ? 
+          Container(
+            width: double.infinity,
+            height: 200,
+            child: Center(
+              child: CircularProgressIndicator(),
+            ),
+          ) 
+        : Form(
+          key: _formkey,
+          child: SingleChildScrollView(
+            padding: EdgeInsets.all(10),
+            child: Column(
+              children: <Widget>[
+                Container(
+                  child: Card(
+                    elevation: 10,
+                    child: Container(
+                      padding: EdgeInsets.all(5),
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          labelText: "Title",
+                          ),
+                          //  initialValue: check.title,
+                        initialValue: newadd ? "" : check.title,
+                        textInputAction: TextInputAction.next,
+                        validator: (value){
+                          if(value.isEmpty){
+                            return "Title can't be empty";
+                          }
+                          return null;
+                        },
+                        onFieldSubmitted: (_){
+                          FocusScope.of(context).requestFocus(_pricefocus);
+                        },
+                        onSaved: (value){
+                          _tempproduct = Product(
+                              id: _tempproduct.id,
+                              description: _tempproduct.description,
+                              price: _tempproduct.price,
+                              title: value,
+                              imageUrl: _tempproduct.imageUrl
+                          );
+                        },
+                      ),
+                    ),
+                  ),
+                ),
+                Card(
                   elevation: 10,
                   child: Container(
                     padding: EdgeInsets.all(5),
                     child: TextFormField(
-                       decoration: InputDecoration(
-                         labelText: "Title",
-                         ),
-                        //  initialValue: check.title,
-                       initialValue: newadd ? "" : check.title,
-                       textInputAction: TextInputAction.next,
-                       validator: (value){
-                         if(value.isEmpty){
-                           return "Title can't be empty";
-                         }
-                         return null;
-                       },
-                       onFieldSubmitted: (_){
-                         FocusScope.of(context).requestFocus(_pricefocus);
-                       },
-                       onSaved: (value){
-                         _tempproduct = Product(
+                      decoration: InputDecoration(labelText: "Price"),
+                      keyboardType: TextInputType.number,
+                      textInputAction: TextInputAction.next,
+                      focusNode: _pricefocus,
+                      initialValue: newadd ? "" : check.price.toString(),
+                      validator: (value){
+                        if(value.isEmpty){
+                          return "Price can't be empty";
+                        }
+                        if(double.parse(value)== null){
+                          return "Enter a valid number";
+                        }
+                        if(double.parse(value)<=0){
+                          return "Price value should be greater than 0";
+                        }
+                        return null;
+                      },
+                      onFieldSubmitted: (_){
+                        FocusScope.of(context).requestFocus(_descriptionfocus);
+                      },
+                      onSaved: (value){
+                          _tempproduct = Product(
+                            id: _tempproduct.id,
+                            description: _tempproduct.description,
+                            price: double.parse(value),
+                            title: _tempproduct.title,
+                            imageUrl: _tempproduct.imageUrl
+                          );
+                        },
+                    ),
+                  ),
+                ),
+                Card(
+                  elevation: 10,
+                  child: Container(
+                    padding: EdgeInsets.all(5),
+                    child: TextFormField(
+                      decoration: InputDecoration(labelText: "Description"),
+                      maxLines: 3,
+                      keyboardType: TextInputType.multiline,
+                      focusNode: _descriptionfocus,
+                      initialValue: newadd ? "" : check.description,
+                      validator: (value){
+                        if(value.isEmpty){
+                          return "This can't be empty";
+                        }
+                        if(value.length <= 10){
+                          return "Description length must be greater than 10 characters";
+                        }
+                        return null;
+                      },
+                      onSaved: (value){
+                          _tempproduct = Product(
+                            id: _tempproduct.id,
+                            description: value,
+                            price: _tempproduct.price,
+                            title: _tempproduct.title,
+                            imageUrl: _tempproduct.imageUrl
+                          );
+                        },
+                    ),
+                  ),
+                ),
+                Card(
+                  elevation: 10,
+                  child: Container(
+                    padding: EdgeInsets.all(5),
+                    child: TextFormField(
+                      decoration: InputDecoration(labelText: "Image Url"),
+                      keyboardType: TextInputType.url,
+                      controller: _imagecontroller,
+                      focusNode: _urlfocus,
+                      initialValue: null,
+                      validator: (value){
+                        if(value.isEmpty){
+                          return "Image url can't be empty";
+                        }
+                        if(!value.startsWith("http") && !value.startsWith("https")){
+                          return "Enter a valid url";
+                        }
+
+                        //  if(!value.endsWith("jpg") && !value.endsWith("png") && !value.endsWith("jpeg")){
+                        //    return "We expect jpg,png,jpeg format only";
+                        //  }
+                                      
+                        return null;
+                      },
+                      onSaved: (value){
+                          _tempproduct = Product(
                             id: _tempproduct.id,
                             description: _tempproduct.description,
                             price: _tempproduct.price,
-                            title: value,
-                            imageUrl: _tempproduct.imageUrl
-                         );
-                       },
+                            title: _tempproduct.title,
+                            imageUrl: value
+                          );
+                        },
                     ),
                   ),
                 ),
-              ),
-              Card(
-                elevation: 10,
-                child: Container(
-                  padding: EdgeInsets.all(5),
-                  child: TextFormField(
-                    decoration: InputDecoration(labelText: "Price"),
-                    keyboardType: TextInputType.number,
-                    textInputAction: TextInputAction.next,
-                    focusNode: _pricefocus,
-                    initialValue: newadd ? "" : check.price.toString(),
-                    validator: (value){
-                      if(value.isEmpty){
-                        return "Price can't be empty";
-                      }
-                      if(double.parse(value)== null){
-                        return "Enter a valid number";
-                      }
-                      if(double.parse(value)<=0){
-                        return "Price value should be greater than 0";
-                      }
-                      return null;
-                    },
-                    onFieldSubmitted: (_){
-                      FocusScope.of(context).requestFocus(_descriptionfocus);
-                    },
-                    onSaved: (value){
-                        _tempproduct = Product(
-                          id: _tempproduct.id,
-                          description: _tempproduct.description,
-                          price: double.parse(value),
-                          title: _tempproduct.title,
-                          imageUrl: _tempproduct.imageUrl
-                        );
-                      },
-                  ),
-                ),
-              ),
-              Card(
-                elevation: 10,
-                child: Container(
-                  padding: EdgeInsets.all(5),
-                  child: TextFormField(
-                     decoration: InputDecoration(labelText: "Description"),
-                     maxLines: 3,
-                     keyboardType: TextInputType.multiline,
-                     focusNode: _descriptionfocus,
-                     initialValue: newadd ? "" : check.description,
-                     validator: (value){
-                       if(value.isEmpty){
-                         return "This can't be empty";
-                       }
-                       if(value.length <= 10){
-                         return "Description length must be greater than 10 characters";
-                       }
-                       return null;
-                     },
-                     onSaved: (value){
-                        _tempproduct = Product(
-                          id: _tempproduct.id,
-                          description: value,
-                          price: _tempproduct.price,
-                          title: _tempproduct.title,
-                          imageUrl: _tempproduct.imageUrl
-                        );
-                      },
-                  ),
-                ),
-              ),
-              Card(
-                elevation: 10,
-                child: Container(
-                  padding: EdgeInsets.all(5),
-                  child: TextFormField(
-                     decoration: InputDecoration(labelText: "Image Url"),
-                     keyboardType: TextInputType.url,
-                     controller: _imagecontroller,
-                     focusNode: _urlfocus,
-                     initialValue: null,
-                     validator: (value){
-                       if(value.isEmpty){
-                         return "Image url can't be empty";
-                       }
-                       if(!value.startsWith("http") && !value.startsWith("https")){
-                         return "Enter a valid url";
-                       }
-
-                      //  if(!value.endsWith("jpg") && !value.endsWith("png") && !value.endsWith("jpeg")){
-                      //    return "We expect jpg,png,jpeg format only";
-                      //  }
-                                    
-                       return null;
-                     },
-                     onSaved: (value){
-                        _tempproduct = Product(
-                          id: _tempproduct.id,
-                          description: _tempproduct.description,
-                          price: _tempproduct.price,
-                          title: _tempproduct.title,
-                          imageUrl: value
-                        );
-                      },
-                  ),
-                ),
-              ),
-              Card(
-                elevation: 10,
-                child: Container(
-                  height: 200,
-                  width: 250,
-                  color: Theme.of(context).scaffoldBackgroundColor,
-                  child: 
-                      Center(
-                        child: _imagecontroller.text.isEmpty 
-                          ? Text(
-                            "Enter a Url",
-                            textAlign: TextAlign.center,
-                          ) 
-                          : FittedBox ( 
-                            child: Image.network(_imagecontroller.text),
-                            fit: BoxFit.cover
+                Card(
+                  elevation: 10,
+                  child: Container(
+                    height: 200,
+                    width: 250,
+                    color: Theme.of(context).scaffoldBackgroundColor,
+                    child: 
+                        Container(
+                          width: 240,
+                          height: 190,
+                          child: Center(
+                            child: _imagecontroller.text.isEmpty 
+                              ? Text(
+                                "Enter a Url",
+                                textAlign: TextAlign.center,
+                              ) 
+                              : FittedBox ( 
+                                child: Image.network(_imagecontroller.text),
+                                fit: BoxFit.cover
+                              ),
                           ),
-                      ),
-                  )
-               ),
-               Container(
-                 margin: EdgeInsets.all(30),
-                  child: RaisedButton(
-                    child: Padding(
-                      padding: const EdgeInsets.all(15.0),
-                      child: ClipRRect(
-                
-                        borderRadius: BorderRadius.circular(30),
-                        child: Icon(
-                          Icons.save,
-                          color: Theme.of(context).scaffoldBackgroundColor,
-                          size: 50,
+                        ),
+                    )
+                ),
+                Container(
+                  margin: EdgeInsets.all(30),
+                    child: RaisedButton(
+                      child: Padding(
+                        padding: const EdgeInsets.all(15.0),
+                        child: ClipRRect(
+                  
+                          borderRadius: BorderRadius.circular(30),
+                          child: Icon(
+                            Icons.save,
+                            color: Theme.of(context).scaffoldBackgroundColor,
+                            size: 50,
+                          ),
                         ),
                       ),
-                    ),
-                    color: Theme.of(context).primaryColor,
-                    onPressed: _saveproduct,
-                 ),
-               )
-            ],
+                      color: Theme.of(context).primaryColor,
+                      onPressed: _saveproduct,
+                  ),
+                )
+              ],
+            ),
           ),
         ),
-      ),
     );
   }
 }
